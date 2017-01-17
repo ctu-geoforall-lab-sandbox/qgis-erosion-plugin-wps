eb2dd98d2d48338938cb76f7908df9e0acb89249
Jan Zachariáš <jan.zacharias@seznam.cz>
2017-01-17 14:37:26 +0100

    start GRASS, create mapset and location

--- pyerosion/erosionbase.py
+++ pyerosion/erosionbase.py
@@ -1,11 +1,79 @@
 # TODO: header
 
+# Version for Windows
+grass7bin_win = r'C:\\OSGeo4W64\\bin\\grass72.bat'
+#myepsg = '5514'
+myfile = 'E:\\janza\\Documents\\grass_skola\\test_vector\\test_vector.shp'
+#myfile = 'E:\\janza\\Documents\\grass_skola\\raster_test.tif'
+
 class ErosionBase:
     def __init__(self):
-        pass
+        ###########
+        import os
+        import sys
+        import subprocess
+        import shutil
+        import binascii
+        import tempfile
+
+        ########### SOFTWARE
+        grass7bin = grass7bin_win
+
+        startcmd = [grass7bin, '--config', 'path']
+
+        p = subprocess.Popen(startcmd, shell=True,
+        					 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+
+        if p.returncode != 0:
+            print >>sys.stderr, "ERROR: Cannot find GRASS GIS 7 start script (%s)" % startcmd
+            sys.exit(-1)
+        str_out = out.decode("utf-8")
+        gisbase = str_out
+        os.environ['GRASS_SH'] = os.path.join(gisbase, 'msys', 'bin', 'sh.exe')
+
+        # Set GISBASE environment variable
+        os.environ['GISBASE'] = gisbase
+        # define GRASS-Python environment
+        gpydir = os.path.join(gisbase, "etc", "python")
+        sys.path.append(gpydir)
+        gisdb = os.path.join(tempfile.gettempdir(), 'grassdata')
+        try:
+            os.stat(gisdb)
+        except:
+            os.mkdir(gisdb)
+
+        # location/mapset: use random names for batch jobs
+        string_length = 16
+        location = binascii.hexlify(os.urandom(string_length))
+        mapset   = 'PERMANENT'
+        location_path = os.path.join(gisdb, location)
+
+        # Create new location (we assume that grass7bin is in the PATH)
+        #  from EPSG code:
+        #startcmd = grass7bin + ' -c epsg:' + myepsg + ' -e ' + location_path
+        #  from SHAPE or GeoTIFF file
+        startcmd = grass7bin + ' -c ' + myfile + ' -e ' + location_path
+
+        p = subprocess.Popen(startcmd, shell=True,
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if p.returncode != 0:
+            print >>sys.stderr, 'ERROR: %s' % err
+            print >>sys.stderr, 'ERROR: Cannot generate location (%s)' % startcmd
+            sys.exit(-1)
+        else:
+            print 'Created location %s' % location_path
+
 
     def import_data(self):
-        pass
+        import grass.script as gscript
+        import grass.script.setup as gsetup
+
+        ###########
+        # launch session
+        gsetup.init(gisbase, gisdb, location, mapset)
 
     def export_data(self):
-        pass
+        #Remove all temp directory
+        shutil.rmtree(gisdb)
